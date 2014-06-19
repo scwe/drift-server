3b6ae198aadbc3f887595e4dc335a0c21883e853
Scott Weston <weston.scot@gmail.com>
2014-06-20 02:39:23 +1200

    Fixed multiline strings in schema

--- schema.js
+++ schema.js
@@ -12,28 +12,28 @@ client.connect(function(err){
     client.query('')
 });
 
-query = client.query('
-    CREATE TABLE categories ( 
-        category_id serial PRIMARY KEY,
-        user_id integer NOT NULL,
-        name text NOT NULL,
-        colour text NOT NULL
-    );
-    CREATE TABLE users (
-        user_id serial,
-        username text,
-        password text NOT NULL,
-        facebook_id text,
-        PRIMARY KEY (username, user_id)
-    );
-    CREATE TABLE markers (
-        user_id integer NOT NULL,
-        marker_id serial PRIMARY KEY,
-        lat double NOT NULL, 
-        lon double NOT NULL,
-        data text,
-        image bytea,
-    );');
+query = client.query(
+    'CREATE TABLE categories (  ' +
+        'category_id serial PRIMARY KEY,    ' +
+        'user_id integer NOT NULL,  ' +
+        'name text NOT NULL,    ' +
+        'colour text NOT NULL   ' +
+    '); ' +
+    'CREATE TABLE users (   ' +
+        'user_id serial,    ' +
+        'username text, ' +
+        'password text NOT NULL,    ' +
+        'facebook_id text,  ' +
+        'PRIMARY KEY (username, user_id)    ' +
+    '); ' +
+    'CREATE TABLE markers ( ' +
+        'user_id integer NOT NULL,  ' +
+        'marker_id serial PRIMARY KEY,  ' +
+        'lat double NOT NULL,   ' +
+        'lon double NOT NULL,   ' +
+        'data text, ' +
+        'image bytea,   ' +
+    ');');
 
 query.on('end', function(result){
     client.end();
