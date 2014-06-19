259ba68d4f87a4f30db19af9d78a00087d92a91b
Scott Weston <weston.scot@gmail.com>
2014-06-19 21:18:11 +1200

    Outline fo the basic requests that can be made

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ var server = app.listen(port, function(){
 app.post('/login', function(req, res){
     if(!req.body.hasOwnProperty('username') ||
         !req.body.hasOwnProperty('password')){
-        res.statusCose = 400;
+        res.statusCode = 400;
         return res.send('Error 400: Post syntax incorrect');
     }
 
@@ -22,8 +22,52 @@ app.post('/login', function(req, res){
 });
 
 app.post('/signup', function(req, res){
+    if(!req.body.hasOwnProperty('username') ||
+        !req.body.hasOwnProperty('password')){
+        res.statusCode = 400;
+        return res.send('Error 400: Post syntax incorrect');
+    }
+});
+
+app.get('/:username/marker/:marker_id', function(req, res){
+    //make sure that the person is either 'username' or they are facebook friends with 'username'
+    var username = req.params.username;
+    var marker_id = req.params.marker_id;
+
+    if(marker_id === "all"){
+
+    }
+});
+
+app.post('/:username/marker/:marker_id', function(req, res){
+    if(!req.body.hasOwnProperty('category') ||
+        !req.body.hasOwnProperty('lat') ||
+        !req.body.hasOwnProperty('lon') ||
+        !req.body.hasOwnProperty('text')){
+        res.statusCode = 400;
+        return res.send('Error 400: Post syntax incorrect');
+    }
+
 
 });
+
+app.post('/:username/category/:category_id', function(req, res){
+    if(!req.body.hasOwnProperty('name') || 
+        !req.body.hasOwnProperty('colour')){
+        res.statusCode = 400;
+        return res.send('Error 400: Post syntax incorrect');
+    }
+
+});
+
+app.post('/:username/category/:category_id', function(req, res){
+    var username = req.params.username;
+    var category_id = req.params.category_id;
+
+    if(category_id === "all"){
+        
+    }
+})
 
 app.get('/', function(req, res){
     res.send("Something or rather that sshould be here, just for testing");
