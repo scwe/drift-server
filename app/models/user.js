93e6f89e87f3d1db8aa0804d3e85db59f67832a5
Scott Weston <weston.scot@gmail.com>
2014-06-22 01:50:39 +1200

    More gets and posts implemented along with more database methods

--- app/models/user.js
+++ app/models/user.js
@@ -14,10 +14,11 @@ var userSchema = mongoose.Schema({
             name      : String,
             colour    : String,
             markers   : [{       //List of all the markers attached to the category
+                name  : {type: String, default : ""},
                 lat   : Number,
                 lon   : Number,
                 text  : String,
-                image : {data : Buffer, type : String},
+                image : {data : Buffer, contentType : String},
             }],
         }]
     },
@@ -33,6 +34,99 @@ userSchema.methods.generateHash = function(password) {
 userSchema.methods.validPassword = function(password) {
     return bcrypt.compareSync(password, this.local.password);
 };
+
+userSchema.methods.createMarker = function(categoryName, _name, _lat, _lon, _text, _image){
+    var cat = findWithName(this.local.categories, categoryName);
+    if(cat === null){
+        return false;
+    } 
+
+    var newMarker = {lat: _lat, lon : _lon, text: _text, image:_image};
+
+    if(_name){
+        newMarker.push({name : _name});
+    }
+
+    this.local.categories.markers.push(newMarker);
+
+    this.save(function(err){
+        if(err){
+            throw err;
+            return false;
+        }
+
+        return this.local.categories.markers.indexOf(newMarker);
+    });
+};
+
+userSchema.methods.createCategory = function(_name, _colour){
+    var old_cat = findWithName(this.local.categories, _name);
+
+    if(cat !== null){
+        return false;
+    }
+
+    var newCat = {name: _name, colour: _colour, markers: []};
+
+    this.local.categories.push(newCat);
+    this.save(function(err){
+        if(err){
+            throw err;
+            return false;
+        }
+        return this.local.categories.indexOf(newCat);
+    });
+};
+
+userSchema.methods.getCategory = function(name){
+    var cat = findWithName(this.local.categories, name);
+    if(cat === null){
+        return false;
+    }
+
+    return cat;
+};
+
+userSchema.methods.getMarker = function(catName, index, name){
+    var cat = findWithName(this.local.categories, catName);
+
+    if(cat === null){
+        return false;
+    }
+
+    var marker = findWithName(cat, name);
+
+    if(marker === null){  //if we can find it by name, just return that, otherwise use the index
+        return cat[index];
+    }
+
+    return marker;
+}
+
+userSchema.methods.allMarkers = function(catName){
+    var cat = findWithName(this.local.categories, catName);
+    if(cat === null){
+        return false;
+    }
+
+    return cat.markers;
+}
+
+userSchema.methods.allCategories = function(){
+    return this.local.categories;
+}
+
+function findWithName(list, name){
+    for (var i = list.length - 1; i >= 0; i--) {
+        if(list[i].name === name){
+            return list[i];
+        }
+    };
+
+    return null;
+}
+
+userSchema.methods.get
 
 //Make sure that we can convert to JSON
 userSchema.set('toJSON', { getters: true, virtuals: false });
--- app/routes.js
+++ app/routes.js
@@ -1,4 +1,5 @@
 // app/routes.js
+
 module.exports = function(app, passport) {
 
     // =====================================
@@ -46,9 +47,10 @@ module.exports = function(app, passport) {
     // =====================================
     // we will want this protected so you have to be logged in to visit
     // we will use route middleware to verify this (the isLoggedIn function)
-    app.get('/profile', isLoggedIn, function(req, res) {
+    app.get('/profile', is_logged_in, function(req, res) {
         res.render('profile.ejs', {
-            user : req.user // get the user out of session and pass to template
+            user : req.user, // get the user out of session and pass to template
+            json : JSON.stringify(req.user.toJSON())
         });
     });
 
@@ -59,10 +61,91 @@ module.exports = function(app, passport) {
         req.logout();
         res.redirect('/');
     });
+
+    app.post('/:category_name/create_marker', is_logged_in, function(req, res){
+        if( !req.body.hasOwnProperty('lat') ||
+            !req.body.hasOwnProperty('lon') ||
+            !req.body.hasOwnProperty('text')){
+            res.statusCode = 400;
+            return res.send('Error 400: Post syntax incorrect');
+        }
+
+        var user = req.user;
+        var marker;
+        var name = ""
+
+        if(req.body.hasOwnProperty('name')){
+            name = req.body.name;
+        }
+
+        if(req.body.hasOwnProperty('image')){
+            marker = user.createMarker(req.params.category_name, name, req.body.lat, req.body.lon, req.body.text, req.body.image);
+        }else{
+            marker = user.createMarker(req.params.category_name, name, req.body.lat, req.body.lon, req.body.text);
+        }
+
+        return res.send(JSON.stringify(marker));
+    });
+
+    app.post('/create_category', is_logged_in, function(req, res){
+        if(!req.body.hasOwnProperty('name') || 
+            !req.body.hasOwnProperty('colour')){
+            res.statusCode = 400;
+            return res.send('Error 400: Post syntax incorrect');
+        }
+
+        return res.send(JSON.stringify(req.user.createCategory(req.body.name, req.body.colour)));
+
+    });
+
+    app.get('/:category_name/:marker_id', is_logged_in, function(req, res){
+        //make sure that the person is either 'username' or they are facebook friends with 'username'
+        var user = req.user;
+
+        if(req.params.marker_id === "all"){
+            var markers = user.allMarkers(req.params.category_name);
+            return res.send(JSON.stringify(markers));
+        }else{
+            var marker = user.getMarker(req.params.category_name, req.params.marker_id);
+            return res.send(JSON.stringify(marker));
+        }
+    });
+
+
+
+    app.get('//category/:category_id', is_logged_in, function(req, res){
+        var username = req.params.username;
+        var category_id = req.params.category_id;
+
+        if(category_id === "all"){
+            var categories = db.get_all_categories(username);
+            return res.send(JSON.stringify(categories));
+        }else{
+            var category = db.get_category(username, category_id);
+            return res.send(JSON.stringify(category));
+        }
+    });
+
+    app.get('/all', is_logged_in, function(req, res){
+        return res.json(req.user.toJSON());
+    });
+
+    app.get('/facebook/:fb_id', is_logged_in, function(req, res){
+        var fb_id = req.params.fb_id;
+
+        var friend_username = db.get_friend(fb_id);
+
+        return res.send('{\'username\': '+friend_username+'}');
+    });
+
+    app.get('/logout', function(req, res){
+        req.logout();
+        res.redirect('/');
+    });
 };
 
 // route middleware to make sure a user is logged in
-function isLoggedIn(req, res, next) {
+function is_logged_in(req, res, next) {
 
     // if user is authenticated in the session, carry on 
     if (req.isAuthenticated())
@@ -71,73 +154,3 @@ function isLoggedIn(req, res, next) {
     // if they aren't redirect them to the home page
     res.redirect('/login');
 }
-
-/*
-
-app.post('/:username/marker/:marker_id', is_logged_in, function(req, res){
-    if( !req.body.hasOwnProperty('lat') ||
-        !req.body.hasOwnProperty('lon') ||
-        !req.body.hasOwnProperty('text')){
-        res.statusCode = 400;
-        return res.send('Error 400: Post syntax incorrect');
-    }
-
-    if(req.body.hasOwnProperty('image')){
-
-    }else{
-        db.create_marker()
-    }
-    db.create_marker(req.body.category)
-
-    res.send("some json things here");
-});
-
-app.get('/:username/marker/:marker_id', is_logged_in, function(req, res){
-    //make sure that the person is either 'username' or they are facebook friends with 'username'
-    var username = req.params.username;
-    var marker_id = req.params.marker_id;
-
-    if(marker_id === "all"){
-        var markers = db.get_all_markers(username);
-        return res.send(JSON.stringify(markers));
-    }else{
-        var marker = db.get_marker(username, marker_id);
-        return res.send(JSON.stringify(marker));
-    }
-});
-
-app.post('/:username/category/:category_id', is_logged_in, function(req, res){
-    if(!req.body.hasOwnProperty('name') || 
-        !req.body.hasOwnProperty('colour')){
-        res.statusCode = 400;
-        return res.send('Error 400: Post syntax incorrect');
-    }
-
-});
-
-app.get('/:username/category/:category_id', is_logged_in, function(req, res){
-    var username = req.params.username;
-    var category_id = req.params.category_id;
-
-    if(category_id === "all"){
-        var categories = db.get_all_categories(username);
-        return res.send(JSON.stringify(categories));
-    }else{
-        var category = db.get_category(username, category_id);
-        return res.send(JSON.stringify(category));
-    }
-});
-
-app.get('/facebook/:fb_id', is_logged_in, function(req, res){
-    var fb_id = req.params.fb_id;
-
-    var friend_username = db.get_friend(fb_id);
-
-    return res.send('{\'username\': '+friend_username+'}');
-});
-
-app.get('/logout', function(req, res){
-    req.logout();
-    res.redirect('/');
-});
-*/
