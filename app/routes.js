b52669d6a3e09c8545443b8dbe6d7cdae1a69df4
Scott Weston <weston.scot@gmail.com>
2014-06-22 21:56:13 +1200

    Started work separating the web side from the unity side

--- app/routes.js
+++ app/routes.js
@@ -38,7 +38,12 @@ module.exports = function(app, passport) {
     });
 
     // process the login form
-    app.post('/login', passport.authenticate('local-login', {
+    app.post('/login', passport.authenticate('local-login'), function(req, res){
+        return res.redirect('/');
+    });
+
+    // process the login form
+    app.post('/login-web', passport.authenticate('local-login', {
         successRedirect : '/profile', // redirect to the secure profile section
         failureRedirect : '/login', // redirect back to the signup page if there is an error
         failureFlash : true // allow flash messages
