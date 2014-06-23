3807d01b732d6cecdbe074495d30ea7b21206bfa
Scott Weston <weston.scot@gmail.com>
2014-06-23 13:20:44 +1200

    Debug lines

--- app/routes.js
+++ app/routes.js
@@ -183,6 +183,7 @@ module.exports = function(app, passport) {
             if(err){
                 return res.status(400).send("There is no one in the database with that id");
             }
+            console.log("In here the categories are: "+categories);
             return categories;
         });
         //We also need to make sure here that the user and the friend are actually friends
