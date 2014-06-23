fee0927d9ab6dab261331d50f51cb2c02d1b0e49
Scott Weston <weston.scot@gmail.com>
2014-06-23 13:41:26 +1200

    Nevermind, this is getting tirering

--- app/routes.js
+++ app/routes.js
@@ -185,6 +185,7 @@ module.exports = function(app, passport) {
                 return res.status(400).send("There is no one in the database with that id");
             }
             friend_categories = data.categories;
+            console.log("We are going to set the thing to: "+data.categories);
         });
         //We also need to make sure here that the user and the friend are actually friends
 
